6b3105515c1010720d06c630f6cd125cd28b254d
sandesht45 <64426190+sandesht45@users.noreply.github.com>
2020-07-05 20:22:47 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,11 @@
+class Box{
+    private int length,breadth,height;
+    public void setdata(int l,int b,int h)
+    { length=l; breadth=b; height=h;}
+    public void showdata()
+    {
+        System.out.println("L="+length);
+        System.out.println("B="+breadth);
+        System.out.println("H="+height);
+    }
+}
---
+++ Example.java
@@ -0,0 +1,9 @@
+class Example
+{
+    public static void main(String[] args)
+    {
+        Box smallbox=new Box();
+        smallbox.setdata(10,15,20);
+        smallbox.showdata();
+    }
+}
